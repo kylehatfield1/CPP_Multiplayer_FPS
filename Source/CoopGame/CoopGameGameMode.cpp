1a04285e530fb0a5e5a4093548c0dff4d5d7e5a8
Kyle <kylehatfield1@gmail.com>
2020-08-14 01:21:03 -0700

    Init Commit.

---
+++ Source/CoopGame/CoopGameGameMode.cpp
@@ -0,0 +1,10 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "CoopGameGameMode.h"
+#include "CoopGameCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ACoopGameGameMode::ACoopGameGameMode()
+{
+
+}
---
+++ Source/CoopGame/Private/PlayerCharacter.cpp
@@ -0,0 +1,99 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PlayerCharacter.h"
+#include "Components/InputComponent.h"
+#include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/Controller.h"
+
+// Sets default values
+APlayerCharacter::APlayerCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	GetMovementComponent()->GetNavAgentPropertiesRef().bCanCrouch = true;
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 540.0f, 0.0f);
+
+	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+	SpringArmComp->bUsePawnControlRotation = true;
+	SpringArmComp->SetupAttachment(RootComponent);
+
+	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+	CameraComp->bUsePawnControlRotation = false;
+	CameraComp->SetupAttachment(SpringArmComp);
+}
+
+
+// Called when the game starts or when spawned
+void APlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+
+void APlayerCharacter::MoveForward(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		float ControllerYaw = Controller->GetControlRotation().Yaw;
+		FRotator NewRotation = FRotator(0.0f, ControllerYaw, 0.0f);
+
+		FVector ForwardVector = FRotationMatrix(NewRotation).GetUnitAxis(EAxis::X);
+		AddMovementInput(ForwardVector, Value);
+	}
+}
+
+void APlayerCharacter::MoveRight(float Value)
+{
+	if ((Controller != NULL) && (Value != 0.0f))
+	{
+		float ControllerYaw = GetControlRotation().Yaw;
+		FRotator NewRotation = FRotator(0.0f, ControllerYaw, 0.0f);
+
+		FVector RightVector = FRotationMatrix(NewRotation).GetUnitAxis(EAxis::Y);
+		AddMovementInput(RightVector, Value);
+	}
+}
+
+void APlayerCharacter::BeginCrouch()
+{
+	Crouch();
+}
+
+void APlayerCharacter::EndCrouch()
+{
+	UnCrouch();
+}
+
+
+// Called every frame
+void APlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+
+// Called to bind functionality to input
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &APlayerCharacter::BeginCrouch);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &APlayerCharacter::EndCrouch);
+}
+
